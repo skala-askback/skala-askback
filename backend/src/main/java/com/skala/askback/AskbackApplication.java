956ae33245160303a812f18959d7cd0843135901
Minchae <minchaeh217@gmail.com>
2026-09-03 09:35:52 +0900

    feat: BE 프로젝트 생성

---
+++ backend/src/main/java/com/skala/askback/AskbackApplication.java
@@ -0,0 +1,13 @@
+package com.skala.askback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskbackApplication.class, args);
+	}
+
+}
